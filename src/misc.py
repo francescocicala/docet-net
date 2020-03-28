6d9be9838e8da475339cddd0aabfe002a1b34762
Francesco <francesco.cicala00@gmail.com>
2020-03-28 15:35:24 +0100

    add get_state_dict function

--- src/misc.py
+++ src/misc.py
@@ -1,8 +1,16 @@
 import torch
 
 def vectorize_weights(params):
-    pass
+    return torch.cat([torch.flatten(w.data) for w in params])
 
 
-def insert_weights(model, weights_vector):
-    pass
+def get_state_dict(state_dict, weights_vector):
+    i = 0
+    state_dict_keys = list(state_dict.keys())
+    for key in state_dict_keys:
+        old_weights = state_dict[key]
+        p_size = torch.numel(old_weights)
+        new_weights = weights_vector[i : i + p_size].reshape_as(old_weights)
+        state_dict[key] = new_weights
+        i += p_size
+    return state_dict
